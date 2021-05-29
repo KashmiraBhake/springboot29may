4d84bad0b8136ffe6710307ea256efc3ee9472c1
KashmiraBhake <kashmirabhake@gmail.com>
2021-05-29 06:36:32 +0000

    save

---
+++ src/main/java/com/test/webapp/PatientDetails.java
@@ -0,0 +1,51 @@
+package com.test.webapp;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@EnableAutoConfiguration
+public class PatientDetails {
+    @RequestMapping("/")
+    public ModelAndView home() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("home");
+
+        return modelAndView;
+    }
+
+    @RequestMapping("/new_patient")
+    public ModelAndView new_patient() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("new_patient");
+
+        return modelAndView;
+    }
+
+    @RequestMapping("/create_new_patient")
+    public ModelAndView create_new_patient() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("submit");
+
+        return modelAndView;
+    }
+
+    @RequestMapping("/search_patient_form")
+    public ModelAndView search_patient_form() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("search_patient_form");
+
+        return modelAndView;
+    }
+
+    @RequestMapping("/search_patient")
+    public ModelAndView search_patient() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("search_result");
+        modelAndView.addObject("name", "Kashmira");
+
+        return modelAndView;
+    }
+}
